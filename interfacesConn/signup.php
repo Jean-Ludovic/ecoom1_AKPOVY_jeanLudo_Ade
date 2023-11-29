d876d3ba10ce0945b1fa3efc2fe38a78c34dc9c8
Jean-Ludovic <ludotoro02@gmail.comm>
2023-11-29 13:23:58 -0500

    pages

---
+++ interfacesConn/signup.php
@@ -0,0 +1,18 @@
+<h2>Connexion</h2>
+
+
+<form method="post" action="">
+    <input hidden name="action" value="signup">
+    
+    <label for="lname">Nom</label>
+    <input id="lname" name="lname" type="text">
+    <label for="fname">Prenom</label>
+    <input id="fname" name="fname"type="text"> <br> <br>
+    <label for="user_name">Nom d'utilisateur</label>
+    <input id="user_name" name="user_name" type="text"> <br> <br>
+    <label for="email">Email</label>
+    <input id="email" name="email" type="email"> <br> <br>
+    <label for="pwd">Mot de passe</label>
+    <input id="pwd" name="pwd" type="password"> <br> <br>
+    <button type="submit">Sign Up</button>
+</form>
